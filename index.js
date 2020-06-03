ceef213fef3f6a53030961187929b7364b465627
i-mnam <8linkbv@gmail.com>
2020-06-03 11:41:28 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -12,8 +12,15 @@ const handleHome = (req, res) => {
     res.send("Hello From home");
 }
 const handleProfile = (req, res) => res.send("You are on my profile.");
+const betweenHome = (req, res, next) => {
+    console.log("middleware test.");
+    next();
+}
+
 
 // set routes
+app.use(betweenHome); // 응답하기 전 프로세스 처리를 할 수 있으므로 선언 위치가 중요하다.
+//app.get("/", betweenHome, handleHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening); //callback 으로 call
