c7ae54d97c978ed428cc45b0ed0ca02ce774f06a
i-mnam <8linkbv@gmail.com>
2020-06-07 00:45:19 +0900

    #2.16 Local Variables in Pug & using custom middleware

--- app.js
+++ app.js
@@ -8,19 +8,26 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 
+import { localsMiddleware } from "./middlewares";
+
 const app = express();
 
+// 주의) setting order
+app.use(helmet());
+// set application
+app.set("view engine", "pug");
 
 // set middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
-app.use(helmet());
 app.use(logger("dev"));
+app.use(localsMiddleware);
 
-
-// set application
-app.set("view engine", "pug");
+// 복습) use custom middleware
+// app.use(function(req, res, next) {});
+// app.use((req, res, next) => {});
+// const localsMiddleware = (req, res, next) => {}
 
 // set intro-router
 app.use(routes.home, globalRouter);
