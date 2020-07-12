a579a757a085a271c9115dfcdb6d9070f5777b34
i-mnam <8linkbv@gmail.com>
2020-07-13 02:58:00 +0900

    #5.1 Scss: Footer and Login Join

--- middlewares.js
+++ middlewares.js
@@ -1,19 +1,18 @@
 import multer from "multer";
 import routes from "./routes";
 
-
-const multerVideo = multer({dest: "uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
     // 복습) use export : 그래야 다른 곳에서 사용이 가능하니까
-    res.locals.siteName =  "Wetube";
+    res.locals.siteName = "Wetube";
     res.locals.routes = routes;
 
     // temp!
     res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    }
+        isAuthenticated: false,
+        id: 1,
+    };
     next(); // 주의) middleware는 next()를 호출해야 req를 전달 할 수 있음
-}
+};
 export const uploadVideo = multerVideo.single("videoFile");
