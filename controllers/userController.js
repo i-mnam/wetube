a3b3c22c9f1fe17d48f13daea6120ca1733d0154
i-mnam <8linkbv@gmail.com>
2020-07-15 00:56:27 +0900

    #6.2 Local Authentication with Passport part2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,17 @@
-import routes from "../routes"
+import routes from "../routes";
+import User from "../models/User";
 
 //export const join = (req, res) => res.render("join");
 export const getJoin = (req, res) => {
     // res.render(routes.join, {
     res.render("join", {
-        pageTitle: "Join"
+        pageTitle: "Join",
     });
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
     //const obj = req.body;   // obj.name;s
     const {
-        body: {
-            name,
-            email,
-            password,
-            password2
-        }
+        body: { name, email, password, password2 },
     } = req;
 
     // ECMA Script6 TEST
@@ -37,44 +33,59 @@ export const postJoin = (req, res) => {
     if (password !== password2) {
         res.status(400);
         res.render("join", {
-            pageTitle: "Incorrect Join"
+            pageTitle: "Incorrect Join",
         });
     } else {
         // Todo: Register User
-        // Todo: Log user in
-        res.redirect(routes.home);
+
+        // res.redirect(routes.home);
         // res.render("join", {
         //     pageTitle: "Correct Join"
         // });
+        console.log("jfs;ldkfjasl;f//" + name, email, password);
+        try {
+            const user = await User({
+                name,
+                email,
+            });
+            // passport 설정내용이 없어도 작동이 매우 잘되었음..
+            await User.register(user, password);
+        } catch (error) {
+            console.log(error);
+        }
+        // Todo: Log user in
+        res.redirect(routes.home);
     }
 };
 
-export const getLogin = (req, res) => res.render("login", {
-    pageTitle: "Log In"
-});
+export const getLogin = (req, res) =>
+    res.render("login", {
+        pageTitle: "Log In",
+    });
 
 export const postLogin = (req, res) => {
     // Todo: check User Info
     res.redirect(routes.home);
-}
+};
 
 export const logout = (req, res) => {
     // Todo: Process Log Out
     res.redirect(routes.home);
-}
+};
 
 // export const users = (req, res) => res.render("users");
 
-export const editProfile = (req, res) => res.render("editProfile", {
-    pageTitle: "Edit Profile"
-});
+export const editProfile = (req, res) =>
+    res.render("editProfile", {
+        pageTitle: "Edit Profile",
+    });
 export const changePassword = (req, res) => {
     res.render("changePassword", {
-        pageTitle: "Change Password"
+        pageTitle: "Change Password",
     });
-}
+};
 export const userDetail = (req, res) => {
     res.render("userDetail", {
-        pageTitle: "User Detail"
+        pageTitle: "User Detail",
     });
-}
+};
--- passport.js
+++ passport.js
@@ -1,4 +1,8 @@
-import passport from "passport";
-import User from "./models/User";
+// import passport from "passport";
+// import User from "./models/User";
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// passport가 사용자 인증을 처리할 수 있도록 한 설정임.
